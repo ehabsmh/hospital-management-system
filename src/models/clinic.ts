929f82305d8b8ce976ccab3c9b85307f60e0d04e
Ehab Hussein <ehabsmh3@gmail.com>
2025-03-18 23:53:33 +0200

    implement clinic model

---
+++ src/models/clinic.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+
+const clinicSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  floor: {
+    type: Number,
+    required: true,
+  },
+  roomNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+});
+
+const Clinic = model("clinic", clinicSchema);
+
+export default Clinic;
