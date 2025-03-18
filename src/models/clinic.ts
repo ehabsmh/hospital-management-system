b0a787fd25cccfac1ac06cea5e278f97d5b1b51b
Ehab Hussein <ehabsmh3@gmail.com>
2025-03-18 23:55:09 +0200

    delete timestamps and version key from clinic schema

--- src/models/clinic.ts
+++ src/models/clinic.ts
@@ -1,21 +1,24 @@
 import { model, Schema } from "mongoose";
 
-const clinicSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const clinicSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    floor: {
+      type: Number,
+      required: true,
+    },
+    roomNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
   },
-  floor: {
-    type: Number,
-    required: true,
-  },
-  roomNumber: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: false, versionKey: false }
+);
 
 const Clinic = model("clinic", clinicSchema);
 
