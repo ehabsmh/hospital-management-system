bb7c6c0bf6e8252b107aee371ed76c142acb4483
Ehab Hussein <ehabsmh3@gmail.com>
2025-03-19 20:10:40 +0200

    implement editing a clinic

--- src/controllers/clinics.ts
+++ src/controllers/clinics.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import Clinic from "../models/clinic";
-import { DuplicationError, RequireError } from "../utils/errorHandlers";
+import {
+  DuplicationError,
+  NotFoundError,
+  RequireError,
+} from "../utils/errorHandlers";
 
 class ClinicController {
   static async create(req: Request, res: Response) {
@@ -44,6 +48,44 @@ class ClinicController {
     }
   }
 
-  static async update(req: Request, res: Response) {}
+  static async edit(req: Request, res: Response) {
+    const { name, floor, roomNumber } = req.body;
+    const { id } = req.params;
+
+    try {
+      // Check if the clinic changed effectively
+      const clinicChanged = await Clinic.findOne({ _id: id, ...req.body });
+
+      if (clinicChanged) {
+        res.status(400).json({ error: { message: "Nothing changed." } });
+
+        return;
+      }
+
+      const clinic = await Clinic.findByIdAndUpdate(
+        id,
+        { name, floor, roomNumber },
+        { new: true }
+      );
+
+      if (!clinic) throw new NotFoundError("Clinic not found.");
+
+      res.json({ data: clinic, message: "Clinic updated successfully." });
+    } catch (err) {
+      if (err instanceof NotFoundError) {
+        res
+          .status(err.statusCode)
+          .json({ error: { name: err.name, message: err.message } });
+
+        return;
+      }
+
+      if (err instanceof Error) {
+        res.status(500).json({
+          error: { message: "Something went wrong with the server." },
+        });
+      }
+    }
+  }
 }
 export default ClinicController;
