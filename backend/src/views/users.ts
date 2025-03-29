58ab926752282417e6ef1b4a5cf0bb3e815ea0b0
Ehab Hussein <ehabsmh3@gmail.com>
2025-03-30 01:55:29 +0200

    implement endpoint to get the current logged in user

--- backend/src/views/users.ts
+++ backend/src/views/users.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import UserController from "../controllers/users";
+import { auth } from "../middlewares/auth";
 
 const userRouter = express.Router();
 
@@ -7,5 +8,6 @@ userRouter.post("/sign-up", UserController.signup);
 userRouter.put("/create-password", UserController.createPassword);
 userRouter.post("/sign-in", UserController.signin);
 userRouter.post("/logout", UserController.logout);
+userRouter.get("/me", auth, UserController.me);
 
 export default userRouter;
