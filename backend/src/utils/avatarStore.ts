8f55975fa0b4b20c2314fab1ee056a2362ec3083
Ehab Hussein <ehabsmh3@gmail.com>
2025-04-20 22:26:00 +0200

    test avatars

--- backend/src/utils/avatarStore.ts
+++ backend/src/utils/avatarStore.ts
@@ -1,12 +1,13 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/uploads");
+    cb(null, path.resolve(__dirname, "../public/uploads"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.filename + "-" + uniqueSuffix + ".jpg");
+    cb(null, uniqueSuffix + ".jpg"); // Always use .jpg
   },
 });
 
