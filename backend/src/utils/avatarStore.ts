2d9d03abaa6d65c6729f9d9ac1d6521ba75369d7
Ehab Hussein <ehabsmh3@gmail.com>
2025-04-20 22:29:53 +0200

    test avatars

--- backend/src/utils/avatarStore.ts
+++ backend/src/utils/avatarStore.ts
@@ -3,9 +3,11 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    console.log("File received:", file);
     cb(null, path.resolve(__dirname, "../public/uploads"));
   },
   filename: function (req, file, cb) {
+    console.log("File received:", file);
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + ".jpg"); // Always use .jpg
   },
