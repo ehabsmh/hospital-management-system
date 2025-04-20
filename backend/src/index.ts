f6aaeb13b73d2226f57d512cb3023659d3345582
Ehab Hussein <ehabsmh3@gmail.com>
2025-04-20 16:23:43 +0200

    deploying

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ app.use(express.static("public"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.HOST_NAME!],
     credentials: true,
   })
 );
